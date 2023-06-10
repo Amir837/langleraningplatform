5980206eba6af763c6d11be9f5bfd1a6df764b6c
Amir <amir.maksyutov@outlook.com>
2023-06-09 19:07:23 -0700

    Idea of the SentenceComponent

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { SentenceComponent } from './SentenceComponent';
+
 function App() {
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    1-st version is comming soon :)
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <SentenceComponent wordList = {"1-st version is comming soon :)".split(" ")} originalLanguage = "en" translatedLanguage = "ru" />
             </header>
         </div>
     );
