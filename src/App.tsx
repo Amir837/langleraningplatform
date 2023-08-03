e1d9b0ccc86254859f94f649872631673621072e
Amir <amir.maksyutov@outlook.com>
2023-08-02 20:24:25 -0700

    Revert "Revert "Merge pull request #3 from Amir837/Amir837/Migration""
    This reverts commit e4f76a43591c8e517f979cad6455879d9763a820.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './css/App.css';
 
+import { NavMenu } from './NavMenu';
+import { Practice } from './Practice';
 import { SentenceComponent } from './SentenceComponent';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-
-            </header>
+            <NavMenu currentPage="Practice" />
+            <Practice />
         </div>
     );
 }
