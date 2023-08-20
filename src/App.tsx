2d6c3c74cc7b8505bc2eb88d02d3a5c83fc4a773
Amir <amir.maksyutov@outlook.com>
2023-08-20 00:54:17 -0700

    Adding some chat functionality

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ import { NavMenu } from './NavMenu';
 import { TenseChat } from './TenseChat';
 
 import { Practice } from './Practice';
-import {Home} from './Home';
-import {About} from './About';
-import {Contact} from './Contact';
+import { Home } from './Home';
+import { About } from './About';
+import { Contact } from './Contact';
 
 function App() {
   return (
@@ -21,38 +21,38 @@ function App() {
 }
 
 function AppContent() {
-    const [currentPage, setCurrentPage] = useState('Home');
-    const location = useLocation();
-    
-    useEffect(() => {
-      // Определите логику для обновления currentPage в зависимости от текущего пути
-      if (location.pathname === '/Practice') {
-        setCurrentPage('Practice');
-      } else if (location.pathname === '/About') {
-        setCurrentPage('About');
-      } else if (location.pathname === '/Contact') {
-        setCurrentPage('Contact');
-      } else {
-        setCurrentPage('Home');
-      }
-    }, [location.pathname]);
-    
-    return (
-        <div>
-          {/*
+  const [currentPage, setCurrentPage] = useState('Home');
+  const location = useLocation();
+
+  useEffect(() => {
+    // Определите логику для обновления currentPage в зависимости от текущего пути
+    if (location.pathname === '/Practice') {
+      setCurrentPage('Practice');
+    } else if (location.pathname === '/About') {
+      setCurrentPage('About');
+    } else if (location.pathname === '/Contact') {
+      setCurrentPage('Contact');
+    } else {
+      setCurrentPage('Home');
+    }
+  }, [location.pathname]);
+
+  return (
+    <div className="App">
+      {/*
             <div className="App">
                 <NavMenu currentPage={currentPage} />
             </div>
           */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path='/langleraningplatform' element={<Home />} />
-          <Route path="/practice" element={<TenseChat />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-    );
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/langleraningplatform' element={<Home />} />
+        <Route path="/practice" element={<TenseChat />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/TenseChat.tsx
+++ src/TenseChat.tsx
@@ -6,12 +6,57 @@ import { SentenceComponent } from './SentenceComponent';
 import './css/TenseChat.css';
 
 function TenseChat() {
+    interface Message {
+        text: string;
+        className: string;
+    }
+
     const [sentence, setSentence] = useState("She goes to the store to buy earrings.");
+    const [inputValue, setInputValue] = useState("");
+    const [messages, setMessages] = useState<Message[]>([]);
+
+    // temporary
+    var fromTense = "Present Simple";
+    var toTense = "Past Simple";
+
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleKeyPress = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === "Enter") {
+            addNewMessage(messages);
+        }
+    };
+
+    function addNewMessage(prevMessages:Message[]){
+        
+        const newMessage: Message = {
+            text: inputValue,
+            className: 'answer',
+        };
+        setMessages(prevMessages => [...prevMessages, newMessage])
+        setInputValue('');
+        getTeacherFeedback(messages);
+    }
+
+    function getTeacherFeedback(prevMessages:Message[]){
+        return fetch(`https://flashenglish.azurewebsites.net/api/gptconnectionenglishtenseteacher?sentence=${sentence}&userInput=${inputValue}&fromTense=${fromTense}&toTense=${toTense}`)
+        .then(response => response.json())
+        .then(data => {
+            const newMessage: Message = {
+                text: data.text,
+                className: 'question',
+            };
+            setMessages(prevMessages => [...prevMessages,newMessage])
+        })
+    }
 
     return(
         <div className="tenseChatPage">
             <div className="leftBar">
                 <div className="navMenu">
+                    navMenu
                 </div>
 
                 <div className="tenseController">
@@ -24,7 +69,7 @@ function TenseChat() {
             </div>
 
             <div className="chat">
-                <div className="sentenceTitle">
+                <div className="sentenceTitleContainer">
 
                 </div>
 
@@ -32,8 +77,8 @@ function TenseChat() {
 
                 </div>
 
-                <div className="chatInput">
-                    
+                <div className="userInput">
+
                 </div>
             </div>
         </div>
