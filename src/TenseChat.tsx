55aab833a50d1db9ccccbe5fa9abbbfbda887021
Amir <amir.maksyutov@outlook.com>
2023-08-19 20:49:11 -0700

    HTML structure 2

--- src/TenseChat.tsx
+++ src/TenseChat.tsx
@@ -9,8 +9,33 @@ function TenseChat() {
     const [sentence, setSentence] = useState("She goes to the store to buy earrings.");
 
     return(
-        <div className="TenseChatPage">
+        <div className="tenseChatPage">
+            <div className="leftBar">
+                <div className="navMenu">
+                </div>
 
+                <div className="tenseController">
+
+                </div>
+
+                <div className="something">
+
+                </div>
+            </div>
+
+            <div className="chat">
+                <div className="sentenceTitle">
+
+                </div>
+
+                <div className="messageHistory">
+
+                </div>
+
+                <div className="chatInput">
+                    
+                </div>
+            </div>
         </div>
     );
 }
