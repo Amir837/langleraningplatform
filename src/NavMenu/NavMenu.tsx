f856d8c9c77f7aa3fd32540b47dc102158beb6b0
maksmarat <maksmarat164@yandex.ru>
2023-10-04 21:15:40 +0700

    add mobile menu (but with some bugs)

--- src/NavMenu/NavMenu.tsx
+++ src/NavMenu/NavMenu.tsx
@@ -1,5 +1,5 @@
 /* Navigation menu as a functional component */
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './NavMenu.css';
@@ -10,6 +10,12 @@ interface NavMenuProps {
 }
 
 function NavMenu(props: NavMenuProps) {
+    const [ifopenMenu, setIfopenMenu] = useState<boolean>(false);
+
+    const openMenu = () => {
+        setIfopenMenu(!ifopenMenu);
+    }
+
     var f = "/" + `${props.currentPage}`;
     if (props.currentPage === "Home") f = "/";
     return (
@@ -38,12 +44,33 @@ function NavMenu(props: NavMenuProps) {
                     </div>
                 </nav>
             </div>
+
             <div className="collapsed_navMenu">
             <h1><Link to={f}>{props.currentPage}</Link></h1>
-                <div className="mobile-menu"><svg width="24" height="18" xmlns="http://www.w3.org/2000/svg"><path d="M24 16v2H0v-2h24zm0-8v2H0V8h24zm0-8v2H0V0h24z" fill="#FFF" fill-rule="evenodd" /></svg></div>
+                <button className="mobile-menu " onClick={openMenu}><svg width="24" height="18" xmlns="http://www.w3.org/2000/svg"><path d="M24 16v2H0v-2h24zm0-8v2H0V8h24zm0-8v2H0V0h24z" fill="#FFF" fill-rule="evenodd" /></svg></button>
             </div>
+
+            {ifopenMenu ? (
+                <nav>
+                    <ul className="menuUl-mobile">
+                        <li className={`menuUl-item ${props.currentPage === "Home" ? "menuUl-active-link" : "menuUl-active-item"}`}>
+                            <Link className="menuUl-link temp-size" to="/">Home <span className="temp-notImplemented">(Not Implemented)</span></Link> { /* Home is not implemented yet */}
+                        </li>
+                        <li className={`menuUl-item ${props.currentPage === "Practice" ? "menuUl-active-link" : "menuUl-active-item"}`}>
+                            <Link className="menuUl-link temp-size" to="/Practice">Practice</Link>
+                        </li>
+                        <li className={`menuUl-item ${props.currentPage === "About" ? "menuUl-active-link" : "menuUl-active-item"}`}>
+                            <Link className="menuUl-link temp-size" to="/Practice">About <span className="temp-notImplemented">(Not Implemented)</span></Link> { /* About is not implemented yet */}
+                        </li>
+                        <li className={`menuUl-item ${props.currentPage === "Feedback" ? "menuUl-active-link" : "menuUl-active-item"}`}>
+                            <Link className="menuUl-link temp-size" to="/Feedback">Feedback</Link>
+                        </li>
+                    </ul>
+                </nav>
+            ) : null}
+
         </div>
     );
-}
+} 
 
 export { NavMenu };
